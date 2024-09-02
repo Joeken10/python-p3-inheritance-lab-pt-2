ade2237ee3ac5af68be392e82d0364e79800f04f
Joeken10 <joekentafir@gmail.com>
2024-09-02 22:23:03 +0300

    add files

--- lib/student.py
+++ lib/student.py
@@ -3,10 +3,18 @@ class Student:
         print ("Hey there! I'm so excited to learn stuff.")
 
     def raises_hand(self):
-        print()
+        print("pick me !")
         
         pass
     
 
 class ChattyStudent(Student):
+    def hello(self):
+        super().hello()
+        print ("How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...")
+
+    def raises_hand(self):
+        for _ in range(10):
+            super().raises_hand()
+    
     pass
