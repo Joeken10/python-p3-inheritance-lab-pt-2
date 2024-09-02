9841cbe851251c4ac25a16312ef546abc44f21c8
Joeken10 <joekentafir@gmail.com>
2024-09-02 22:27:50 +0300

    add files

--- lib/student.py
+++ lib/student.py
@@ -2,8 +2,8 @@ class Student:
     def hello(self):
         print ("Hey there! I'm so excited to learn stuff.")
 
-    def raises_hand(self):
-        print("pick me !")
+    def raise_hand(self):
+        print ("Pick me!")
         
         pass
     
